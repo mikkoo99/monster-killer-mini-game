718477b12fe045eb9ad9b4db2f60d43cd5551e7f
Michał Olesiak <mikko@michas-macbook-air.home>
2022-03-15 20:06:43 +0100

    another commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,77 @@
+const playerAttackValue = 10;
+const strongAttackValue = playerAttackValue * 1.5;
+const monsterAttackValue = 14;
+const healValue = 20;
 
+const eneteredValue = prompt('Set health Value');
+
+let chosenMaxLife = parseInt(eneteredValue);
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+    chosenMaxLife = 100;
+}
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
+
+//Set health
+adjustHealthBars(chosenMaxLife);
+function reset() {
+    currentPlayerHealth = chosenMaxLife;
+    currentMonsterHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+}
+
+function endRound() {
+    const initialPlayerHealth = currentPlayerHealth;
+    const monsterDamage = dealPlayerDamage(monsterAttackValue);
+    currentPlayerHealth -= monsterDamage;
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth);
+        alert('your bonus life had saved you');
+    }
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You win');
+        reset();
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lose');
+        reset();
+    }
+}
+
+function attackMonster(type) {
+    let maxDamage;
+    if (type === 'ATTACK') {
+        maxDamage = playerAttackValue;
+    } else if (type === 'STRONG_ATTACK') {
+        maxDamage = strongAttackValue;
+    }
+    const playerDamage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= playerDamage;
+    endRound();
+}
+
+function attackHandler() {
+    healPotion = 0;
+    attackMonster('ATTACK');
+}
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK');
+}
+
+function healHandler() {
+    let setLife;
+    if (currentPlayerHealth >= chosenMaxLife - healValue) {
+        setLife = chosenMaxLife - currentPlayerHealth;
+    } else {
+        setLife = healValue;
+    }
+    increasePlayerHealth(setLife);
+    currentPlayerHealth += setLife;
+    endRound();
+}
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healHandler);
